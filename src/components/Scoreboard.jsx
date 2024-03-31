1c4955e6a54064245f00cd1a93165db7a92095d1
Dr. Kato Samuel Namuene <kato.namuene@ubuea.cm>
2024-03-31 06:54:17 +0100

    Add src/components/Scoreboard.jsx

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -6,7 +6,6 @@ const Scoreboard = ({ currentScore, bestScore }) => {
   return (
     <div className="scoreboard">
       <div className="score">Current Score: {currentScore}</div>
-      <div className="scoreTitle">SCOREBOARD</div>
       <div className="score">Best Score: {bestScore}</div>
     </div>
   );
